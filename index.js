28c7e6a39a9ebbdd98f93a83446219526c664881
Renato Zanella <renatozr07@gmail.com>
2022-11-12 00:23:09 -0300

    feat: "/" route res

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (_req, res) => res.status(200).send('Welcome to Memories API'));
 
 const PORT = process.env.PORT || 5000;
 
